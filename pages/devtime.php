f99376dac9588f4c2542eff46c86251d7a8f14b4
Christian <info@devowl.de>
2018-05-18 10:29:14 +0200

    Added placeholder page with basic layout

---
+++ pages/devtime.php
@@ -0,0 +1,11 @@
+<?php
+	#header
+	if( !defined( 'MANTIS_VERSION' ) ) { exit(); }
+	layout_page_header( plugin_lang_get( 'title' ) );
+	layout_page_begin();
+?>
+
+Hello World
+<?php layout_page_end(); 
+	#footer
+?>
